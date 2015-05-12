8edd30bd75ed7fcbc9faef84d85a3559eac64ca6
James Conkling <james.lane.conkling@gmail.com>
2015-05-12 12:28:44 -0400

    change default background brightness to 100% [most bright]

--- js/id/ui/background.js
+++ js/id/ui/background.js
@@ -7,7 +7,7 @@ iD.ui.Background = function(context) {
             ['right', [-1, 0]],
             ['bottom', [0, 1]]],
         opacityDefault = (context.storage('background-opacity') !== null) ?
-            (+context.storage('background-opacity')) : 0.5,
+            (+context.storage('background-opacity')) : 1,
         customTemplate = context.storage('background-custom-template') || '';
 
     // Can be 0 from <1.3.0 use or due to issue #1923.
